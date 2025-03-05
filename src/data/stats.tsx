bd7756d91218344ce9bb3e99088f8f1683b37ac3
LinCie <aldiofernanda@protonmail.com>
2025-03-05 11:30:38 +0700

    feat: adjust stats

--- src/data/stats.tsx
+++ src/data/stats.tsx
@@ -5,18 +5,18 @@ import { IStats } from "@/types";
 
 export const stats: IStats[] = [
     {
-        title: "20M+",
+        title: "5M+",
         icon: <BsBarChartFill size={34} className="text-blue-500" />,
-        description: "Transactions processed securely every day, providing real-time insights."
+        description: "Operations processed securely every day, providing real-time insights."
     },
     {
         title: "5.0",
         icon: <BsFillStarFill size={34} className="text-yellow-500" />,
-        description: "Star rating, consistently maintained across app stores."
+        description: "Star rating, consistently maintained."
     },
     {
-        title: "200+ ",
+        title: "50+ ",
         icon: <PiGlobeFill size={34} className="text-green-600" />,
-        description: "Financial Institutions, seamlessly integrated, so you can manage all accounts."
+        description: "Businesses, all happy and profitting."
     }
 ];
